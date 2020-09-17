a6d162559e1a8a600b88df08c785589b22694a7b
azu-b <azucena.barraza@wizeline.com>
2020-09-17 17:15:13 -0500

    feat: Make styled-components work

---
+++ src/pages/Home/Home.page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Title, Subtitle } from './Home.styled';
+
+const Home = () => (
+  <main>
+    <Title>React Certification 2020</Title>
+    <Subtitle>Welcome to Azu&apos;s challenge</Subtitle>
+  </main>
+);
+
+export default Home;
---
+++ src/pages/Home/Home.styled.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 40px;
+  color: grey;
+`;
+
+const Subtitle = styled.h4`
+  font-size: 16px;
+  color: black;
+`;
+
+export { Title, Subtitle };
