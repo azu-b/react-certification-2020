e96f2d38df2f8e20385e59aa3e64ad9b5d605c63
azu-b <azucena.barraza@wizeline.com>
2020-09-22 14:45:21 -0500

    feat: Make YouTube API work

--- src/pages/Home/Home.page.jsx
+++ src/pages/Home/Home.page.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import { Title, Subtitle } from './Home.styled';
+import youtube from '../../utils/youtube';
 
-const Home = () => (
-  <main>
-    <Title>React Certification 2020</Title>
-    <Subtitle>Welcome to Azu&apos;s challenge</Subtitle>
-  </main>
-);
+const Home = () => {
+  const response = youtube.get('/search', {
+    params: {
+      q: 'cats',
+    },
+  });
+  console.log(response);
+
+  return (
+    <main>
+      <Title>React Certification 2020</Title>
+      <Subtitle>Welcome to Azu&apos;s challenge</Subtitle>
+    </main>
+  );
+};
 
 export default Home;
---
+++ src/utils/youtube.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+import YOUTUBE_API_KEY from './env';
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3/',
+  params: {
+    part: 'snippet',
+    maxResults: 4,
+    key: YOUTUBE_API_KEY,
+  },
+});
