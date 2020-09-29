8d3018da2a7b208f99e4dda39736c0550065e4ee
azu-b <azucena.barraza@wizeline.com>
2020-09-28 22:33:48 -0500

    feat: Make real Home page

--- src/components/SearchResults/SearchResults.styled.js
+++ src/components/SearchResults/SearchResults.styled.js
@@ -4,6 +4,13 @@ const Container = styled.div`
   display: flex;
   align-items: flex-start;
   justify-content: flex-start;
+  flex-direction: column;
+  width: 100%;
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+    flex-wrap: wrap;
+  }
 `;
 
 export { Container };
--- src/components/VideoCard/VideoCard.styled.js
+++ src/components/VideoCard/VideoCard.styled.js
@@ -6,9 +6,23 @@ const Card = styled.div`
   align-items: flex-start;
   width: 320px;
   height: 320px;
-  border: 1px solid grey;
-  overflow-y: hidden;
-  margin: 15px;
+  border-top: 1px solid grey;
+  border-bottom: 1px solid grey;
+  overflow: hidden;
+  margin: 15px 0;
+
+  @media (min-width: 768px) {
+    border: 1px solid grey;
+    margin: 30px;
+  }
+
+  @media (min-width: 1024px) {
+    margin: 10px;
+  }
+
+  @media (min-width: 1440px) {
+    margin: 20px;
+  }
 `;
 
 const Thumbnail = styled.img`
@@ -17,12 +31,14 @@ const Thumbnail = styled.img`
 `;
 
 const Title = styled.h3`
+  font-family: 'Folks Normal';
   color: black;
   padding: 10px 5px;
   margin: 0;
 `;
 
 const Description = styled.p`
+  font-family: 'Arial';
   color: grey;
   font-size: 13px;
   padding: 10px 5px;
