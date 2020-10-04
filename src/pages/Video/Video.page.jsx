bc89452a26d54e5551ccbfae0fddc5bac0a2c06e
Azu Barraza <41596861+azu-b@users.noreply.github.com>
2020-10-03 22:29:30 -0500

    refactor: Address 1st Delivery comments (#8)
    * refactor: Address first comments in 1st delivery
    
    - Change the way to write the Routes
    - Change the icons folder location
    - Add the useEffect to use YouTube API
    
    * refactor: Make function to clean YouTube response
    
    * chore: Create _redirects file

--- src/pages/Video/Video.page.jsx
+++ src/pages/Video/Video.page.jsx
@@ -1,36 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-// import VideoComponent from '../../components/Video';
+import VideoComponent from '../../components/Video';
 import { Container } from './Video.styled';
-import youtube from '../../utils/youtube';
+import { YouTubeAPI, cleanYouTubeResponse } from '../../utils/youtube';
 
 const Video = () => {
   const { id: videoId } = useParams();
   const [video, setVideo] = useState(undefined);
+  const [isLoading, setIsLoading] = useState(true);
 
-  const getVideoInfo = async () => {
-    try {
-      const response = await youtube.get('/videos', {
-        params: {
-          id: videoId,
-        },
-      });
-      console.log(response.data.items[0]);
-      setVideo(response.data.items[0]);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  useEffect(() => {
+    const getVideoInfo = async () => {
+      try {
+        const response = await YouTubeAPI.get('/videos', {
+          params: {
+            id: videoId,
+          },
+        });
+        const cleanVideos = cleanYouTubeResponse(response.data.items);
+        setVideo(cleanVideos[0]);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
-  getVideoInfo();
+    getVideoInfo();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <Container>
+      {isLoading && <div>Loading video info...</div>}
       {video ? (
-        <Video
+        <VideoComponent
           id={videoId}
-          title={video.snippet.title}
-          description={video.snippet.description}
+          title={video.title}
+          description={video.description}
         />
       ) : (
         <div>Ups! No video with this id found</div>
