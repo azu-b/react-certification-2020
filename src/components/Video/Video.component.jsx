39610cf50939e936334f5e883ef338cebfbfda54
azu-b <azucena.barraza@wizeline.com>
2020-10-06 07:22:46 -0500

    feat: Add Favorites button to Video component

--- src/components/Video/Video.component.jsx
+++ src/components/Video/Video.component.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
-import { Container, VideoContainer, Title, Description, Error } from './Video.styled';
+import {
+  Container,
+  VideoContainer,
+  Title,
+  Description,
+  Error,
+  FavoriteButton,
+} from './Video.styled';
 
 const Video = (props) => {
-  const { id, title, description } = props || null;
+  const { id, title, description, authenticated = true } = props || null;
+  const action = (isFavorite) => (!isFavorite ? 'Remove from' : 'Add to');
 
   return id && title && description ? (
     <Container>
       <VideoContainer data-testid="video" src={`https://www.youtube.com/embed/${id}`} />
       <Title data-testid="title">{title}</Title>
       <Description data-testid="description">{description}</Description>
+      {authenticated && <FavoriteButton add>{action(false)} Favorites</FavoriteButton>}
     </Container>
   ) : (
     <Error>No video information was provided</Error>
